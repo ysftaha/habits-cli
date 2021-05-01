dadbc2f7685761bf4ca4a4dd5e16bcc50a38d883
ysftaha <tahay@uwindsor.ca>
2021-05-01 15:17:13 -0400

    fixed usage header.

--- hbtcli.c
+++ hbtcli.c
@@ -15,34 +15,33 @@ habit create(const char *hbt_name, const bool is_freq, const char *rec,
              const char *date);
 
 void usage() {
-  puts("Usage: hcli (-a | -r) habit -d dd/mm/yy (-R repeat | -F "
-       "frequency_list)\n"
-       "       hcli -l\n" // list habits in db
+  puts("Usage: hcli (-a | -d) habit -s DD/MM/YY (-r <num> | -f <0-6>)\n"
        "       hcli -v\n" // version
        "       hcli -h\n" // help
+       "       hcli -l\n" // list habits in db
        "Options:\n"
-			 "    -v               Show version.\n"
-			 "    -h               This help message and usage.\n"
-			 "    -l               List habit names in database and general info.\n"
-			 "    -a <habit-name>  Add a habit.\n"
-			 "    -s <DD/MM/YY>    Set starting date (inclusive) of tracking habit\n"
-			 "                     when adding a habit.\n"
-			 "    -r <num>         Set repeatition when adding a habit.\n"
-			 "                     Example: hcli -a habit-name -r 7 -s DD/MM/YY.\n"
-			 "                     Note: '-r' and '-f' are mutually exclusive.\n"
-			 "    -f <0-6>         Set frequency when adding a habit. Where 0 is\n"
-			 "                     the starting day of the week.\n"
-			 "                     Example: hcli -a habit-name -f 013 -s DD/MM/YY.\n"
-			 "                              This adds a habit that repeats every\n"
-			 "                              Monday(0), Tuesday(1), Thursday(3).\n"
-			 "                     Note: '-r' and '-f' are mutually exclusive.\n"
-			 "    -d <name>        Delete a habit 'name' if it exists.\n"
-			 );
+       "    -v               Show version.\n"
+       "    -h               This help message and usage.\n"
+       "    -l               List habit names in database and general info.\n"
+       "    -a <habit-name>  Add a habit.\n"
+       "    -s <DD/MM/YY>    Set starting date (inclusive) of tracking habit\n"
+       "                     when adding a habit.\n"
+       "    -r <num>         Set repeatition when adding a habit.\n"
+       "                     Example: hcli -a habit-name -r 7 -s DD/MM/YY.\n"
+       "                     Note: '-r' and '-f' are mutually exclusive.\n"
+       "    -f <0-6>         Set frequency when adding a habit. Where 0 is\n"
+       "                     the starting day of the week.\n"
+       "                     Example: hcli -a habit-name -f 013 -s DD/MM/YY.\n"
+       "                              This adds a habit that repeats every\n"
+       "                              Monday(0), Tuesday(1), Thursday(3).\n"
+       "                     Note: '-r' and '-f' are mutually exclusive.\n"
+       "    -d <name>        Delete a habit 'name' if it exists.\n");
 }
 
-void version() {
-  puts("hcli pre-alpha");
-}
+/**
+ * @brief prints the current verion of hcli.
+ */
+void version() { puts("hcli pre-alpha"); }
 
 int main(int argc, char *argv[]) {
   char opt;
@@ -54,15 +53,15 @@ int main(int argc, char *argv[]) {
     switch (opt) {
     case 'v':
       version();
-			break;
+      break;
     case 'h':
       usage();
-			break;
+      break;
     case '?':
     default:
-			puts("[Err] Unrecognized argument");
+      puts("[Err] Unrecognized argument");
       usage();
-			break;
+      break;
     }
   }
 }
